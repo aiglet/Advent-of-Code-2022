65d472fe95338cf98fa55cdfbc53121d855032f9
aiglet <aiglet@gmail.com>
2022-12-02 21:55:44 -0500

    01-01 works! Hurray for try/catch!

--- Day-01/01-01.py
+++ Day-01/01-01.py
@@ -1,5 +1,5 @@
 # Open the data file
-data = open("test.txt")
+data = open("input.txt")
 lines = data.readlines()
 
 # Start the math
@@ -9,23 +9,29 @@ def calorie_counter(lines):
     big_elf = 0
     current_calories = 0
 
-# Go through the data file
+# For each line in the data file
 
     for line in lines:
-        if "\n" in line:
-            if current_calories > big_elf:
-                big_elf == current_calories
-                current_calories == 0
-            else:
-                current_calories == 0
-        else:
+
+        # Check to see if you can convert the line to an integer
+        try:
+            int(line)
+
+            # If you can, add the integer value of the line to current_calories
             current_calories += int(line)
 
-    print(big_elf)
+            # Check to see if the current total is larger than big_elf and replace it if true
+            if current_calories > big_elf:
+                big_elf = current_calories
 
-# For each line, if it has data, add it to the current total
-# If the line is blank, compare the current total to BigElf
-# If the current total is bigger, replace BigElf with that total
-# Zero out the current total and move to the next line
+        # If you *cannot* convert the line to an integer
+        except ValueError:
+
+            # Reset current_calories to 0 and continue to the next item on the list
+            current_calories = 0
+            continue
+
+
+    print(f"The big elf has {big_elf} calories.")
 
 calorie_counter(lines)

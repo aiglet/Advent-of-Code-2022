7b9eec89ad5fee9d6844756a3fd006cbb6ae1638
aiglet <aiglet@gmail.com>
2022-12-03 21:21:43 -0500

    * Add Day 02 Part 02 problem * Working 02-02 code!

---
+++ Day-02/02-02.py
@@ -0,0 +1,36 @@
+# Open the file and split it into lines
+data = open("input.txt")
+lines = data.readlines()
+
+
+def better_roshambo(plays):
+    total_score = 0
+
+    # Iterate through all my lines
+    for line in plays:
+        # Play each round and add the resulting total to the total score each time
+        if line[0] == "A":
+            if line[2] == "X":
+                total_score += 3
+            elif line[2] == "Y":
+                total_score += 4
+            elif line[2] == "Z":
+                total_score += 8
+        elif line[0] == "B":
+            if line[2] == "X":
+                total_score += 1
+            elif line[2] == "Y":
+                total_score += 5
+            elif line[2] == "Z":
+                total_score += 9
+        elif line[0] == "C":
+            if line[2] == "X":
+                total_score += 2
+            elif line[2] == "Y":
+                total_score += 6
+            elif line[2] == "Z":
+                total_score += 7
+
+    print(f"The final score is {total_score}")
+
+better_roshambo(lines)

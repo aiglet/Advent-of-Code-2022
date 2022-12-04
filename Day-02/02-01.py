ded38673191c7f0cf5580b6a6d4b4d293aa5b104
aiglet <aiglet@gmail.com>
2022-12-03 21:02:08 -0500

    * Added print statements to try to figure out where my logic went wrong. Still can't figure it out. Going to start over with a different approach.

--- Day-02/02-01.py
+++ Day-02/02-01.py
@@ -17,18 +17,24 @@ def roshambo(lines):
         # Calculate their score
         if line[0] == "A":
             their_score += 1
+            print("They played Rock.")
         elif line[0] == "B":
             their_score += 2
+            print("They played Paper.")
         elif line[0] == "C":
             their_score += 3
+            print("They played Scissors.")
 
         # Calculate my score
         if line[2] == "X":
             my_score += 1
+            print("I played Rock.")
         elif line[2] == "Y":
             my_score += 2
+            print("I played Paper.")
         elif line[2] == "Z":
             my_score += 3
+            print("I played Scissors.")
 
         # Add my score to the total
         total += my_score
@@ -36,10 +42,14 @@ def roshambo(lines):
         # Figure out who won
         if my_score > their_score:
             total += 6
+            print(f"I won. My total score is {total}.")
         elif my_score == their_score:
             total += 3
+            print(f"We tied. My total score is {total}.")
         elif my_score < their_score:
             total += 0
+            print(f"I lost. My total score is {total}.")
+
 
         my_score = 0
         their_score = 0
